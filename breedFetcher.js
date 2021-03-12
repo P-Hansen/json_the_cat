3f346b95093b21aa39adad854160f676a8e743f4
P-hansen <philip.pt.hansen@gmail.com>
2021-03-12 04:01:41 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require("request");
 const getMeACatHere = "https://api.thecatapi.com/v1/breeds/search?q=";
-const whatBreed = process.argv.slice(2);
 
-const breedFetcher = (address, breed) => {
-  request(address + breed, (error, body) => {
+const breedFetcher = (breed, callBack) => {
+  request(getMeACatHere + breed, (error, response, body) => {
     if (error !== null) {
-      console.log("Sorry an error occured\n" + error);
-      return;
+      //console.log("Sorry an error occured\n" + error);
+      callBack(error, null);
+    } else {
+      //console.log(body);
+      const data = JSON.parse(body);
+      if (body === "[]") {
+        //console.log(`Sorry there are no ${breed}'s found in the database.`);
+        callBack(`Sorry there are no ${breed}'s found in the database.`, null);
+      } else {
+        //console.log(data[0]["description"]);
+        callBack(error, data[0]["description"]);
+      }
     }
-    //console.log(body);
-    const data = JSON.parse(body);
-    if (body === "[]") {
-      console.log(`Sorry there are no ${breed}'s found in the database.`);
-      return;
-    }
-    console.log(data[0]["description"]);
   });
 };
 
-breedFetcher(getMeACatHere, whatBreed[0]);
+module.exports = breedFetcher;
